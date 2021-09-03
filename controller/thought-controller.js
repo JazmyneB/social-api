8d0b77deca86030a84da677bf434052f5271b7ab
JazmyneB <bradleyj1@email.arizona.edu>
2021-09-02 18:26:46 -0700

    trying to fix deleteReaction

--- controller/thought-controller.js
+++ controller/thought-controller.js
@@ -70,7 +70,7 @@ const thoughtController = {
     },
     removeReaction({ params }, res) {
         Thought.findOneAndUpdate(
-            { _id: params.id },
+            { _id: params.thoughtId },
             { $pull: { reactions: { reactionId: params.reactionId } } },
             { new: true }
         )
