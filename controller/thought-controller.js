0c3d6d3bb3d7b52d0192e272cfa36ae437ef1b46
JazmyneB <bradleyj1@email.arizona.edu>
2021-09-02 18:43:56 -0700

    still having removeReaction error

--- controller/thought-controller.js
+++ controller/thought-controller.js
@@ -71,7 +71,7 @@ const thoughtController = {
     removeReaction({ params }, res) {
         Thought.findOneAndUpdate(
             { _id: params.thoughtId },
-            { $pull: { reactions: { reactionId: params.reactionId } } },
+            { $pull: { reactions: params.reactionId } },
             { new: true }
         )
         .then(dbThoughtData => res.json(dbThoughtData))
